598614dadbaf69fa85113e45a93365a46a266357
Samuel Lazzaro <96868382+SamuelLazzaro@users.noreply.github.com>
2023-11-10 08:41:41 +0100

    Sistemati problemi NAVBAR.
    Sistemato problema animazione pulsante MENU e problema background-color degli elementi <li> della class="phone_ul".

--- JS/header_navbar.js
+++ JS/header_navbar.js
@@ -1,11 +1,11 @@
 // Expand & Collaps phone navbar
-// $("#menu_button_div").on("mouseenter", function(){
-//   $("#menu_button_div").addClass("menu_button_div_hover");
+// $(".menu_button_div").on("mouseenter", function(){
+//   $(".menu_button_div").addClass("menu_button_div_hover");
 //   $(".menu_button_line").prop("background-color", "#F9F5F6");
 // });
 
-// $("#menu_button_div").on("mouseleave", function(){
-//   $("#menu_button_div").removeClass("menu_button_div_hover");
+// $(".menu_button_div").on("mouseleave", function(){
+//   $(".menu_button_div").removeClass("menu_button_div_hover");
 //   $(".menu_button_line").removeProp("background-color");
 // });
 
@@ -15,17 +15,29 @@ function isTouchDevice() {
      (navigator.msMaxTouchPoints > 0));
 }
 
-$("#menu_button_div").on("click", function(){
+$(".menu_button_div").on("click", function(){
   $("#show_hide_phone_navbar").slideToggle();
-  $(".menu_button_line#top").toggleClass("menu_top_line");
-  $(".menu_button_line#center").toggleClass("menu_center_line");
-  $(".menu_button_line#bottom").toggleClass("menu_bottom_line");
+
+  if($(".menu_button_div").hasClass("three_lines")){
+    $(".menu_button_line#top").toggleClass("menu_top_line");
+    $(".menu_button_line#center").toggleClass("menu_center_line");
+    $(".menu_button_line#bottom").toggleClass("menu_bottom_line");
+    $(".menu_button_div").removeClass("three_lines");
+  }else{
+    $(".menu_button_line#top").toggleClass("menu_top_line_X");
+    $(".menu_button_line#center").toggleClass("menu_center_line_X");
+    $(".menu_button_line#bottom").toggleClass("menu_bottom_line_X");
+  }
+
+
+  // $(".menu_button_div").toggleClass("menu_button_clicked");
+  // $(".menu_button_line").toggleClass("menu_button_clicked_line");
 
   if(isTouchDevice() == true){
-    $("#menu_button_div").css("background-color", "#A555EC");
+    $(".menu_button_div").css("background-color", "#A555EC");
     $(".menu_button_line").css("background-color", "#F8E8EE");
     setTimeout(function(){
-      $("#menu_button_div").removeAttr("style");
+      $(".menu_button_div").removeAttr("style");
       $(".menu_button_line").removeAttr("style");
     }, 100);
   }
@@ -41,7 +53,7 @@ function resize_scroll() {
   if (document.querySelector(".content_div").getBoundingClientRect().top <= 66) {
     // if($(window).width() <= 913){                             // NON usare .css("width") perche' va in errore (vedi console nel caso)
 
-    if($("#menu_button_div").css("display") != "none"){
+    if($(".menu_button_div").css("display") != "none"){
       $(".menu_bar_div").css("position", "fixed");
       $(".menu_bar_div").css("top", "10px");
       $(".menu_bar_div").css("right", "10px");
