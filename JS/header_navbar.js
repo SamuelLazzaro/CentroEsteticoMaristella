6481f135d47d85419928f2af37068bbc79dad1ab
Samuel Lazzaro <96868382+SamuelLazzaro@users.noreply.github.com>
2023-12-28 09:12:31 +0100

    Test per problema touch device

--- JS/header_navbar.js
+++ JS/header_navbar.js
@@ -9,16 +9,12 @@
 //   $(".menu_button_line").removeProp("background-color");
 // });
 
-var XisPressed = false;
-
 function isTouchDevice() {
   return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
 }
 
 $(".menu_button_div").on("click", function(){
   $("#show_hide_phone_navbar").slideToggle(500);  // Set 500ms for slideUp & slideDown
-
-  XisPressed = false;
 
   if($(".menu_button_div").hasClass("three_lines")){
     $(".menu_button_line#top").toggleClass("menu_top_line");
@@ -29,21 +25,6 @@ $(".menu_button_div").on("click", function(){
     $(".menu_button_line#top").toggleClass("menu_top_line_X");
     $(".menu_button_line#center").toggleClass("menu_center_line_X");
     $(".menu_button_line#bottom").toggleClass("menu_bottom_line_X");
-  }
-
-  //Non so se può servire quando premo la X
-  if($(".menu_button_line#top").hasClass("menu_top_line_X")){
-    // $(".menu_bar_div").removeAttr("style");   // Rimuovo l'inline-css aggiunto nell'if
-    $(".content_div").removeAttr("style");
-    // $(".navbar_background").remove();
-    // $(".navbar_background").removeAttr("style");
-    // NavBarBack_isAdded = false;
-    XisPressed = true;
-
-    console.log("\n");
-    console.log("Close X: " + document.querySelector(".content_div").getBoundingClientRect().top);
-    console.log("\n");
-
   }
 
 
@@ -79,22 +60,10 @@ var NavBarBack_isAdded = false;
 
 function resize_scroll() {
 
-  
-  var menu_bar_div_height = $(".menu_bar_div").outerHeight(true);   // height + margin + padding on top & bottom
+  // console.log(document.querySelector(".content_div").getBoundingClientRect().top);
 
-  const topDistance = 10; //px
-
-  var content_div_top = document.querySelector(".content_div").getBoundingClientRect().top;
-  var menu_bar_div_top = document.querySelector(".menu_bar_div").getBoundingClientRect().top;
-
-  console.log("\n\n");
-  console.log("BEFORE: " + content_div_top);
-  console.log("Menu height: " + menu_bar_div_height);
-
-  if (content_div_top <= (menu_bar_div_height + topDistance) && menu_bar_div_top <= topDistance) {
+  if (document.querySelector(".content_div").getBoundingClientRect().top <= 66) {
     // if($(window).width() <= 913){                             // NON usare .css("width") perche' va in errore (vedi console nel caso)
-
-    console.log("IF: " + document.querySelector(".content_div").getBoundingClientRect().top);
 
     if($(".menu_button_div").css("display") != "none"){
       $(".menu_bar_div").css("position", "fixed");
@@ -102,20 +71,11 @@ function resize_scroll() {
       $(".menu_bar_div").css("right", "10px");
       $(".menu_bar_div").css("left", "10px");
 
-      if(XisPressed == false){
-        $(".content_div").css("margin-top", menu_bar_div_height + topDistance);
-        console.log("X is NOT pressed");
-      }
-
-      console.log("AFTER: " + document.querySelector(".content_div").getBoundingClientRect().top);
+      $(".content_div").css("margin-top", 66);
 
       $(".navbar_background").css("visibility", "visible");
 
     } else {
-      console.log("Else 1");
-
-      XisPressed = false;
-
       $(".menu_bar_div").removeAttr("style");   // Rimuovo l'inline-css aggiunto nell'if
       $(".content_div").removeAttr("style");
       // $(".navbar_background").remove();
@@ -123,10 +83,6 @@ function resize_scroll() {
       // NavBarBack_isAdded = false;
     }  
   } else {
-    console.log("Else 2");
-
-    XisPressed = false;
-
     $(".menu_bar_div").removeAttr("style");   // Rimuovo l'inline-css aggiunto nell'if
     $(".content_div").removeAttr("style");
     // $(".navbar_background").remove();
